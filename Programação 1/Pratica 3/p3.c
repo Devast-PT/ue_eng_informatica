05b64799d9c669db01ffe337434472c79d88413c
EC2 Default User <ec2-user@ip-172-31-22-87.eu-west-2.compute.internal>
2021-10-26 05:16:03 +0000

    Pratica 3

--- Programação 1/Pratica 3/p3.c
+++ Programação 1/Pratica 3/p3.c
@@ -4,11 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
 
 //declarar funçoes
 
 int pergunta2(void);
 int pergunta3(void);
+int pergunta4(void);
+int pergunta5(void);
+int pergunta7(void);
+int pergunta8(void);
+int pergunta9(void);
+int pergunta10(void);
+int pergunta11(void);
+int pergunta12(void);
+
 
 //main
 
@@ -21,6 +31,14 @@ int main(int argc, char *argv[]){
             printf("\n\n\n*========================*\n");
             printf("| 2 - Pergunta 2         |\n");
             printf("| 3 - Pergunta 3         |\n");
+            printf("| 4 - Pergunta 4         |\n");
+            printf("| 5 - Pergunta 5         |\n");
+            printf("| 7 - Pergunta 7         |\n");
+            printf("| 8 - Pergunta 8         |\n");
+            printf("| 9 - Pergunta 9         |\n");
+            printf("| A - Pergunta 10        |\n");
+            printf("| B - Pergunta 11        |\n");
+            printf("| C - Pergunta 12        |\n");            
             printf("| Q - Sair Programa      |\n");
             printf("*========================*\n");
             printf("Porfavor insira o numero da pergunta que pretende selecionar: ");
@@ -37,15 +55,35 @@ int main(int argc, char *argv[]){
         switch(userInput){
             case 50:
                 pergunta2();
-                printf("\nObrigado e Volte sempre\n");
-                check = 0;
                 break;
             case 51:
                 pergunta3();
-                printf("\nObrigado e Volte sempre\n");
-                check = 0;
                 break;
-            
+            case 52:
+                pergunta4();
+                break;
+            case 53:
+                pergunta5();
+                break;
+            case 55:
+                pergunta7();
+                break;
+            case 56:
+                pergunta8();
+                break;
+            case 57:
+                pergunta9();
+                break;
+            case 65:
+                pergunta10();
+                break;
+            case 66:
+                pergunta11();
+                break;
+            case 67:
+                pergunta12();
+                break;                   
+                                
             case 81:
             case 113:
                 printf("Obrigado e Volte sempre\n");
@@ -102,6 +140,128 @@ int pergunta3(void){
     
     dist = 0.5 * ((vel * vel) / (10 * 10));
     printf("A uma velocidade de %i km\\h, a sua distancia de travagem é %.2F metros", (int) vel, dist);
+    
+    return 0;
+}
+
+int pergunta4(void){
+    //variaveis
+    int userInput = 0;
+    
+    //input inteiro, output raiz quadrada desse numero
+    
+    printf("Qual o numero? ");
+    scanf(" %i", &userInput);
+    if (userInput < 0){
+        printf("O numero inserido e negativo.\n");
+        return 0;
+    }
+    userInput = sqrt(userInput);
+    printf("A raiz quadrada e %i\n", userInput);
+    sleep(2);
+    
+    return 0;
+}
+
+int pergunta5(void){
+    int num_1, num_2, num_3;
+    
+    printf("numero 1: ");
+    scanf(" %i", &num_1);
+    printf("numero 2: ");
+    scanf(" %i", &num_2);
+    printf("numero 3: ");
+    scanf(" %i", &num_3);
+
+    if ( num_1 > num_2 && num_1 < num_3){
+        printf("E o numero é %i\n", num_1);
+    } else if (num_2 > num_1 && num_2 < num_3){
+        printf("E o numero é %i\n", num_2);
+    } else {
+        printf("E o numero é %i\n", num_3);
+    }
+    sleep(1.5);
+    return 0;
+}
+
+int pergunta7(void){
+    float altura_input;
+    
+    printf("Qual a altura da pessoa? ");
+    scanf("%f", &altura_input);
+    
+    if (altura_input >= 1.3 && altura_input < 1.6){
+        printf("Essa pessoa é baixa!\n");
+        sleep(1.5);
+        return 0;
+    } else if (altura_input >= 1.6 && altura_input < 1.75){
+        printf("Essa pessoa é mediana!\n");
+        sleep(1.5);
+        return 0;        
+    }else if (altura_input >=1.75 && altura_input < 1.9){
+        printf("Essa pessoa é alta!\n");
+        sleep(1.5);
+        return 0;       
+    }else if (altura_input > 1.9){
+        printf("Essa pessoa é altissima!\n");
+        sleep(1.5);
+        return 0;       
+    } else {
+        printf("Essa pessoa é baixissima!\n");
+        sleep(1.5);
+    }
+    
+    return 0;
+}
+
+int pergunta8(void){
+    int numero_utilizador[2];
+    
+    printf("Indique os numeros 1 e 2: ");
+    scanf("%i %i", &numero_utilizador[0], &numero_utilizador[1]);
+    
+    if ( (numero_utilizador[0]%numero_utilizador[1]) == 0){
+        printf(" %i é multiplo de %i\n", numero_utilizador[0], numero_utilizador[1]);
+        sleep(1.5);
+    } else {
+        printf(" %i nao é multiplo de %i\n", numero_utilizador[0], numero_utilizador[1]);
+        sleep(1.5);
+    }
+    
+    return 0;
+}
+
+int pergunta9(void){
+    int numero_3algo;
+    int check = 1;
+    while (check){
+        printf("Insira um numero de tres algorismos ");
+        scanf(" %i", &numero_3algo);
+        if (numero_3algo < 1000 && numero_3algo > 99){
+            check = 0;
+        }
+    }
+    if ((numero_3algo/100) == (numero_3algo%10)){
+        printf("%i é capicua\n", numero_3algo);
+    } else {
+        printf("%i nao é capicua\n", numero_3algo);
+    }
+    sleep(1.5);
+    
+    return 0;
+}
+
+int pergunta10(void){
+    
+    return 0;
+}
+
+int pergunta11(void){
+    
+    return 0;
+}
+
+int pergunta12(void){
     
     return 0;
 }
